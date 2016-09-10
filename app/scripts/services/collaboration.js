168fb267906f1ea7e045bdbc328562e60a3b8a50
juergen1976 <githenn26>
2016-09-10 17:15:30 +0200

    Adapt to new firebase version

--- app/scripts/services/collaboration.js
+++ app/scripts/services/collaboration.js
@@ -3,7 +3,18 @@
 angular.module('smartcollaborateApp')
   .factory("collaborations", ["$firebaseArray",
     function($firebaseArray) {
-      var ref = new Firebase("https://smartcollaboration.firebaseio.com/collaborations");
+
+
+      var config = {
+        apiKey: "AIzaSyDjysQMnu5y7B0GNBuHmR_absjFUFbWxdE",
+        authDomain: "smartcollaboration.firebaseapp.com",
+        databaseURL: "https://smartcollaboration.firebaseio.com",
+        storageBucket: "firebase-smartcollaboration.appspot.com",
+      };
+      firebase.initializeApp(config);
+      var ref = firebase.database().ref();
+
+
       return $firebaseArray(ref);
     }
   ]);
